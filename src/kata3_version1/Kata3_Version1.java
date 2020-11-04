28c69bcfa5ac947fa7c312e6da62d1093e17fe1b
aitan <aitan@LAPTOP-5TR53I5R>
2020-11-04 11:51:58 +0000

    Kata3_Version1

---
+++ src/kata3_version1/Kata3_Version1.java
@@ -0,0 +1,12 @@
+package kata3_version1;
+
+public class Kata3_Version1 {
+
+    public static void main(String[] args) {
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM"); 
+        histogramDisplay.execute();
+        
+    }
+    
+}
